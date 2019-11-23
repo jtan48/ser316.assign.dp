75991def6efecd41c123b1a0359aa52be4575e7b
Jerry Tan <jtan48@asu.edu>
2019-11-23 02:12:43 -0500

    Initial revision

---
+++ src/main/java/blue/flame/Ass5.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package blue.flame;
+
+/**
+ * @author jerry
+ *
+ */
+public class Ass5 {
+
+	/**
+	 * 
+	 */
+	public Ass5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("what hath been wrought?");
+
+	}//main
+
+}//class
