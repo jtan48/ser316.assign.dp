5afab9f2f59093e0074f8f53e912876e74951611
Jerry Tan <jtan48@asu.edu>
2019-11-23 21:09:19 -0500

    Updates Receiver object in preparation for implementating the requirements of fighting and gaining the powers of the defeated foe.

--- src/main/java/blue/flame/Receiver.java
+++ src/main/java/blue/flame/Receiver.java
@@ -27,25 +27,36 @@ public class Receiver implements Ass5K {
 		
 		Iterator<Villain> iter = villains.iterator();
 		Iterator<Hero> hero_iter = her0es.iterator();
+		Villain defendant = iter.next();
+		Hero prosecutor = hero_iter.next();
+		int counter = 1;
+		//System.out.println("ROUND " + counter++);
 		while(iter.hasNext()) {
-			Villain defendant = iter.next();
-			Hero prosecutor = hero_iter.next();
-			
+
+			System.out.println("ROUND " + counter++);
+
 			// announcer stuff.. and in this corner, etc etc
 			System.out.println(new StringBuffer(prosecutor.getName()).append(" versus ").append(defendant.getName()));
 			System.out.println(FIGHT);
 			System.out.println(new StringBuffer(prosecutor.getName()).append(" stats: ").append(prosecutor.toString()));
-			System.out.println(new StringBuffer(prosecutor.getName()).append(" stats: ").append(prosecutor.toString()));
-			
-			
-			
-			
+			System.out.println(new StringBuffer(defendant.getName()).append(" stats: ").append(defendant.toString()));
+
+			// this is it..
+			// this is where the proverbial horse is buried..
+			if(prosecutor.getStrength() > defendant.getStrength()) { // prosecutor wins -- got a conviction
+				prosecutor.setStrength(prosecutor.getStrength() + defendant.getStrength());
+				prosecutor.setSmarts(prosecutor.getSmarts() + defendant.getSmarts());
+				defendant = iter.next();
+				if(null == defendant) { return LIGHT; }
+			} else { // defendant wins -- got an acquittal
+				defendant.setStrength(defendant.getStrength() + prosecutor.getStrength());
+				defendant.setSmarts(defendant.getSmarts() + prosecutor.getSmarts());
+				prosecutor = hero_iter.next();
+				if(null == prosecutor) { return DARKNESS; }
+			}//if else
 		}//while
 		
-		
-		
-		
-		
+		// default realism
 		return DARKNESS;
 	}//fight
 	
